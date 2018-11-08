f3637dc8f02f8fd7e76af47c9367646491d80e81
Dima Gerasimov <karlicoss@gmail.com>
2018-11-08 08:02:38 +0000

    Proper backup

--- binstapaper.py
+++ binstapaper.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
+import json
+import sys
+
 from instapaper import Instapaper
 
 from instapaper_secrets import *
 
 LIMIT = 500
 
-api = Instapaper(OAUTH_ID, OAUTH_SECRET)
-api.login(USERNAME, PASSWORD)
-# TODO 
-for f in ['unread', 'archive']:
-    bm = api.bookmarks_raw(folder='unread', limit=LIMIT)
-    print(bm)
+# on first login, execute:
+if False:
+    USERNAME = None
+    PASSWORD = None
 
+    from instapaper import Instapaper
+    api = Instapaper(OAUTH_ID, OAUTH_SECRET)
+    odata = api.login(USERNAME, PASSWORD)
+    print("paste this into your secrets file")
+    print(repr(odata))
+    sys.exit(0)
+
+api = Instapaper(OAUTH_ID, OAUTH_SECRET)
+api.login_with_token(**OAUTH_PARAMS)
+
+res = {
+    'bookmarks': [],
+    'highlights': [],
+}
+for f in ['unread', 'archive']:
+    bm = api.bookmarks_raw(folder=f, limit=LIMIT)
+    del bm['user']
+    for k, l in bm.items():
+        res[k].extend(l)
+
+
+json.dump(res, sys.stdout, ensure_ascii=False, indent=1, sort_keys=True)
