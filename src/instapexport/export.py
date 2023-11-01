eab2a5847776133ec803e3d16eb2e543daf3d35e
karlicoss <karlicoss@gmail.com>
2023-11-01 21:10:02 +0000

    update exporthelpers & sync with pymplate

--- src/instapexport/export.py
+++ src/instapexport/export.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-import argparse
 import json
 from typing import List
 
-from .exporthelpers.export_helper import Json
+from .exporthelpers.export_helper import Json, setup_parser, Parser
 
 # NOTE: uses custom version (has some changes that are not in upstream yet)
 # https://github.com/karlicoss/instapaper
-import instapaper # type: ignore[import]
+import instapaper  # type: ignore[import-untyped]
 instapaper._API_VERSION_ = "api/1.1"
 # see https://github.com/rsgalloway/instapaper/issues/11
 
 
 def get_json(
-        oauth_id: str,
-        oauth_secret: str,
-        oauth_token: str,
-        oauth_token_secret: str,
+    oauth_id: str,
+    oauth_secret: str,
+    oauth_token: str,
+    oauth_token_secret: str,
 ) -> Json:
     LIMIT = 500
     # default limit is something stupid
@@ -27,7 +26,11 @@ def get_json(
 
     user_folders = api.folders()
     folders: List[str] = [
-        'unread', 'archive', 'starred', # default, as per api docs
+        ## default, as per api docs
+        'unread',
+        'archive',
+        'starred',
+        ##
         *(str(f['folder_id']) for f in user_folders),
     ]
     bookmarks = {}
@@ -40,12 +43,14 @@ def get_json(
     }
 
 
-def login():
+def login() -> None:
     print("NOTE: You'll need your username/password once in order to get oauth_token")
+    # fmt: off
     oauth_id     = input('Your ouath_id: ')
     oauth_secret = input('Your ouath_secret: ')
     username     = input('Your username: ')
     password     = input('Your password: ')
+    # fmt: on
 
     api = instapaper.Instapaper(oauth_id, oauth_secret)
     odata = api.login(username, password)
@@ -53,7 +58,7 @@ def login():
     print(odata)
 
 
-def main():
+def main() -> None:
     parser = make_parser()
     args = parser.parse_args()
 
@@ -70,10 +75,11 @@ def main():
 
 
 def make_parser():
-    from .exporthelpers.export_helper import setup_parser, Parser
-    parser = Parser("""
+    parser = Parser(
+        """
 Export your personal Instapaper data: bookmarked articles and highlights.
-""")
+""",
+    )
     setup_parser(
         parser=parser,
         params=[
@@ -84,11 +90,16 @@ Export your personal Instapaper data: bookmarked articles and highlights.
         ],
         extra_usage='''
 You can also import ~instapexport.export~ as a module and call ~get_json~ function directly to get raw JSON.
-''')
-    parser.add_argument('--login', action='store_true', help='''
+''',
+    )
+    parser.add_argument(
+        '--login',
+        action='store_true',
+        help='''
     Note: OAUTH_ID/OAUTH_SECRET have to be requrested by email
     https://www.instapaper.com/main/request_oauth_consumer_token
-    ''')
+    ''',
+    )
     return parser
 
 
